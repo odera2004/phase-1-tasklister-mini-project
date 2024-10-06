8d696b749ec31488e4e591bc7b961582c09f724c
odera <eugeneodera59@gmail.com>
2024-10-06 10:52:03 +0300

    add text

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskForm = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+  taskForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const  taskDescription = document.getElementById("new-task-description").value;
+     
+    const taskItem = document.createElement("li");
+    taskItem.textContent = taskDescription;
+
+     const deleteButton = document.createElement("button");
+     deleteButton.textContent = "X";
+     deleteButton.className = "delete-button";
+
+     deleteButton.addEventListener('click', ()=> {
+      taskList.removeChild(taskItem);
+     });
+     taskItem.appendChild(deleteButton);
+     taskList.appendChild(taskItem);
+    taskForm.reset();
+  })
 });
